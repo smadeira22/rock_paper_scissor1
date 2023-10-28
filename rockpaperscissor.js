63b64f1043ee03f1b0cdcd380d183e4177e1a9e4
smadeira22 <saramariaramosmadeira@gmail.com>
2023-10-28 01:12:11 +0100

    modify reckpaperscissoe.js file

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -5,15 +5,17 @@ const color = require('ansi-colors');
 
 let i = 1;
 let j = 1;
-            const arrForm = ['rock','paper','scissor'];
+const arrForm = ['rock','paper','scissor'];
 let wins = 0;
 let losses = 0;
 let userFormSecond;
-let userFailed = 0;
+let userFailed;
 let result;
+
     
 while (i>0){
-    if(j === 1){
+    let attempts = 3;
+    if(j === 1){  
         console.log('Hey there! Lets play rock paper scissor');
         const letsPlay = prompt('Press 1 to continue or Press 2 to exit: ');   
         if(letsPlay ==='2'){
@@ -22,6 +24,7 @@ while (i>0){
         }
     }       
     else if(j === -1){
+        userFailed = 0;
         console.log();
         console.log('Hey there! Lets play again!');
     }
@@ -31,35 +34,45 @@ while (i>0){
     const userForm = prompt('Please choose one of the three forms (rock/paper/scissor):  ');
 
     if(userForm != 'rock' && userForm != 'paper' && userForm != 'scissor'){
-        let verd = true;
+        let different = true;
         userFailed =1;
         count = 0;
         console.log(color.red('You selected an invalid input'));
        
-        while(verd === true){
+        while(different === true){
+            
+            console.log(color.red(`You only have ${attempts} attempts!`));
+
             const userForm1 = prompt('Please choose again(rock/paper/scissor):  ');
+
             if(userForm1 === 'rock' || userForm1 === 'paper' || userForm1 === 'scissor'){
-                verd = false;
-                userFormSecond = userForm1;
-            } else count++;
+                different = false;
+                
+            } else count++; attempts--;
             if(count>2){
                 console.log('You have reached the maxium attempts');
                 console.log('You will now exit the program');
                 return null;
             }
+            userFormSecond = userForm1;
         }
     }
-  
-    if(userFailed === 1){
-        const computerForm = arrForm[Math.floor(Math.random() * arrForm.length)];
-        result = determineWinner(computerForm, userFormSecond);
-    } 
 
     const computerForm = arrForm[Math.floor(Math.random() * arrForm.length)];
-    result = determineWinner(computerForm, userForm);
+    if(userFailed === 1){
+        result = determineWinner(computerForm, userFormSecond);
+    } else if(userFailed != 1) {
+          result = determineWinner(computerForm, userForm);
+    }
     console.log();
+    
+    if(userFailed === 1){
+        console.log(`Your choice: ${userFormSecond}`);
+    }
+    else if(userFailed != 1){
+        console.log(`Your choice: ${userForm}`);
+    }
 
-    console.log(`Your choice: ${userForm}`);
     console.log(`Computer choice: ${computerForm}`);
     console.log(result);
 
@@ -72,6 +85,7 @@ while (i>0){
     console.log();
     console.log(`Your results: Wins -> ${wins} and Losses -> ${losses}`)
 
+    console.log();
     const letsPlay2 = prompt('Press 1 to play again or Press 2 to exit: ');
     
 
